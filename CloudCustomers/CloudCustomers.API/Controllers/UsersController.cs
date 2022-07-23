04767f96a587a24bcc88f6729934fa2b8331b0d1
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-07-24 00:12:15 +0100

    TDD for Get_OnSuccess_InvoikesUserServiceExactlyOnce

--- CloudCustomers/CloudCustomers.API/Controllers/UsersController.cs
+++ CloudCustomers/CloudCustomers.API/Controllers/UsersController.cs
@@ -4,15 +4,25 @@ namespace CloudCustomers.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+
 public class UsersController : ControllerBase
 {
+    private readonly IUsersService _userService;
+    private object @object;
 
-    public UsersController() {
+    public UsersController(IUsersService userService) {
+        _userService = userService;
+    }
+
+    public UsersController(object @object)
+    {
+        this.@object = @object;
     }
 
     [HttpGet(Name = "GetUsers")]
 
     public async Task<IActionResult> Get() {
+        var users = await _userService.GetAllUsers();
         return Ok("all good");
     }
 }
--- CloudCustomers/CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
+++ CloudCustomers/CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -1,6 +1,8 @@
 using CloudCustomers.API.Controllers;
+using CloudCustomers.API.Models;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Xunit;
 
 namespace CloudCustomers.UnitTests.Systems.Controllers;
@@ -9,13 +11,40 @@ public class TestUsersController
 {
     [Fact]
     public async Task Get_OnSuccess_ReturnsStatusCode200() {
-      // Arrange
-      var sut = new UsersController();
+        // Arrange
+        var mockUsersService = new Mock<IUsersService>();
 
-      // Act
-      var result = (OkObjectResult)await sut.Get();
+        mockUsersService
+            .Setup(service => service.GetAllUsers())
+            .ReturnsAsync(new List<User>());
 
-      // Assert
-      result.StatusCode.Should().Be(200);
+        var sut = new UsersController(mockUsersService.Object);
+
+        // Act
+        var result = (OkObjectResult)await sut.Get();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task Get_OnSuccess_InvoikesUserServiceExactlyOnce() {
+        // Arrange
+        var mockUsersService = new Mock<IUsersService>();
+
+        mockUsersService
+            .Setup(service => service.GetAllUsers())
+            .ReturnsAsync(new List<User>());
+
+        var sut = new UsersController(mockUsersService.Object);
+
+        // Act
+        var result = await sut.Get();
+
+        // Assert
+        mockUsersService.Verify(
+            service => service.GetAllUsers(),
+            Times.Once()
+        );
     }
 }
